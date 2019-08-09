3b2960b9edfef3fe1d2ca1fc2b06556cc8e6e6b9
LFnando135 <2517160198lgutierrezr@gmail.com>
2019-08-09 12:15:55 -0500

    cambios

---
+++ resources/views/Personal/ListaPersonal.blade.php
@@ -0,0 +1,135 @@
+@extends('template.header')
+
+
+@section('content_header')
+<h4 class="page-title">Personal</h4>
+
+@stop
+
+
+@section('content')
+
+
+<body onload="buscar();">
+<div class="row"><!-- Inicio ROW-->
+	<div class="col-md-12"><!-- Inicio de columna de row -->
+		<div class="card"><!-- inicio de cuerpo card -->
+			<!-- Cabecera titulo -->
+			<div class="card-header">
+				<div class="card-title">Lista Personal</div>
+
+			</div><!-- fin cabecera   -->
+			<div class="card-body">
+				<center>
+					<div class="col-md-4">
+						<label>Nombre(s)</label>
+						<input required="" type="text" class="form-control success" id="NombreB" name="NombreB" placeholder="Nombre(s)" onkeypress="buscar()" onautocomplete="buscar()" onkeyup="buscar()">
+					</div>
+				</center>
+					<br>
+				<div id="tabla" name="tabla">
+					
+				</div>
+			</div>
+
+
+			</div><!-- fin cabecera   -->
+
+
+		</div><!-- Fin de cuerpo de card -->
+	</div><!-- fin columna card -->
+
+
+</div><!-- Fin ROW-->
+
+
+@endsection
+
+@section('jscustom')
+<script type="text/javascript">
+
+
+function buscar(){
+		document.getElementById('tabla').style.display = 'block';
+        var Nombre= document.getElementById('NombreB').value;
+        //alert("{{url('/Consulta/Personal')}}/"+Bienes+"/"+Marcas);
+
+        $.get("{{url('/Consulta/Personal')}}/"+Nombre, function(data){
+          if (data == "No hay datos") {
+            html="";
+            mensaje("warning","Sin resultados");
+
+          }else{ 
+
+          var html = '<table class="table" >'
+                    +' <thead class="bg-info" style=" overflow: 0;" ><tr>'
+                    +'<th scope="col" style="color:white;">Nombre</th>'
+                    +'<th scope="col" style="color:white;">Email</th>'
+                    +'<th scope="col" style="color:white;">Fecha Nacimiento</th>'
+                    +'<th scope="col" style="color:white;">Direccion</th>'
+                    +'<th scope="col" style="color:white;">Telefono</th>'
+                    +'<th scope="col" style="color:white;">Eliminar</th>'
+                    +'<th scope="col" style="color:white;">Editar</th></tr></thead>'
+                    +'<tbody style=" overflow: auto;">';
+          for(i=0; i<data.length; i++) {
+             var nombre="'"+data[i].name+"'";
+             var ap="'"+data[i].apellido_P+"'";
+             var am="'"+data[i].apellido_M+"'";
+             var ema="'"+data[i].email+"'";
+             var fn="'"+data[i].fecha_nacimiento+"'";
+             var dire="'"+data[i].direccion+"'";
+             var tel="'"+data[i].telefono+"'";
+          html+='<tr>'+
+                      '<th scope="row">'+data[i].name+" "+data[i].apellido_P+" "+data[i].apellido_M+'</th>'+
+                      '<th scope="row">'+data[i].email+'</th>'+
+                      '<th scope="row">'+data[i].fecha_nacimiento+'</th>'+
+                      '<th scope="row">'+data[i].direccion+'</th>'+
+                      '<th scope="row">'+data[i].telefono+'</th>'+
+                      '</tr>';
+          }
+          html+="</tbody></table>";
+         
+        }
+        $('#tabla').html(html);
+        });
+
+
+}
+
+	
+  
+
+	function mensaje(color,mensaje){
+		if(mensaje=="sin_mensaje"){
+
+		}else{
+			var placementFrom = $('#notify_placement_from option:selected').val();
+			var placementAlign = $('#notify_placement_align option:selected').val();
+			var state =color;
+			var style = $('#notify_style option:selected').val();
+			var content = {};
+
+			content.message = mensaje;
+			content.title = 'Nuevo Personal';
+			if (color == "danger") {
+				content.icon = 'la la-close';
+			} else {
+				content.icon = 'la la-check';
+			}
+			content.url = 'index.html';
+			content.target = '_blank';
+
+			$.notify(content,{
+				type: state,
+				placement: {
+					from: placementFrom,
+					align: placementAlign
+				},
+				time: 1000,
+			});
+		}
+		
+	}
+	
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -77,7 +77,8 @@ Route::get('/comboRoles/Roles','PersonalController@comboRoles')->name('comboRole
 Route::get('/modificar_personal','PersonalController@ModificarPersonalvista')->name('ModificarPersonalvista');//Vista modificar
 Route::get('/Consulta/Personal/{nom?}','PersonalController@ConsultaPersonal')->name('ConsultaPersonal');//Vista modificar
 Route::get('/modificar_personal/modificar/{id?}/{nom?}/{ap?}/{am?}/{direccion?}/{correo?}/{tel?}/{fecha?}','PersonalController@ModificarPersona')->name('vistaModificarPersona');//Guardar Personal
-
+Route::delete('personal/eliminarRegistro/{id}', 'PersonalController@eliminarPersonal')->name('eliminarPersonal');
+Route::get('/listado_personal','PersonalController@vistaPersonal')->name('vistaPersonal');//Vista Registrar
 
 
 
