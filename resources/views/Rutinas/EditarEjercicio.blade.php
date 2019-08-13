29dc79b2ecd625740801625569b910d2706f236e
KikeCamarenaLS <49732143+KikeCamarenaLS@users.noreply.github.com>
2019-08-13 11:36:21 -0500

    lagartijas

--- resources/views/Rutinas/EditarEjercicio.blade.php
+++ resources/views/Rutinas/EditarEjercicio.blade.php
@@ -59,7 +59,13 @@
 				$.get("{{url('/pintar/resultado/combo/Rutinas')}}/"+seleccionado, function(data){ 
 					         var html='<br><br><div class="container" >'+
 '<div class="row justify-content-center" 	>';
+				
           for(i=0; i<data.length; i++) {
+          		var Nombre_Ejercicio="'"+data[i].Nombre_Ejercicio+"'";
+				var Imagen="'"+data[i].Imagen+"'";
+				var Serie="'"+data[i].Serie+"'";
+				var Repeticiones="'"+data[i].Repeticiones+"'";
+				var Descripcion="'"+data[i].Descripcion+"'";
           	html+='<div class="col-md-4 ">'+
 						'<div class="card card-pricing card-pricing-focus card-primary">'+
 							'<div class="card-header">'+
@@ -82,7 +88,7 @@
 											'<span class="name-specification">Descripcion</span>'+
 											'<span class="status-specification">'+data[i].Descripcion+'</span>'+
 										'</li>'+
-									'</ul><br><a href="#" class="btn btn-success" data-toggle="modal" onclick="editarEjercicio()" data-target="#miModalPEs">Editar</a>'+
+									'</ul><br><a href="#" class="btn btn-success" data-toggle="modal" onclick="editarEjercicio('+Nombre_Ejercicio+','+Imagen+','+Serie+','+Repeticiones+','+Descripcion+')" data-target="#miModalPEs">Editar</a>'+
 								'</div>'+
 							/*	'<div class="card-footer">'+
 									'<button class="btn btn-light btn-block"><b>Get Started</b></button>'+
@@ -96,30 +102,30 @@
 			}
 		}
 
-		function editarEjercicio(){
+		function editarEjercicio(Nombre_Ejercicio,Imagen,Serie,Repeticiones,Descripcion){
 			var html='<div class="form-group row " >'+
 						'<div class="col-md-4" >'+
 								'<label>Rutina <span class="required-label">*</span></label>'+
-								'<input  type="text" class="form-control success" id="Rutina" name="Rutina" placeholder="Rutina" >'+
+								'<input  type="text" class="form-control success" id="Rutina" name="Rutina" disabled value="'+Nombre_Ejercicio+'" placeholder="Rutina" >'+
 								
 								'</div>'+
 							'<div class="col-md-4">'+
 								'<label>Series<span class="required-label">*</span></label>'+
-								'<input required="" type="Number" class="form-control" id="Serie" name="Serie" placeholder="Serie" >'+
+								'<input required="" type="Number" class="form-control" id="Serie" name="Serie" value="'+Serie+'" placeholder="Serie" >'+
 							'</div>'+
 							'<div class="col-md-4">'+
 								'<label>Repeticiones<span class="required-label">*</span></label>'+
-								'<input required="" type="Number" class="form-control" id="Repeticion" name="Repeticion" placeholder="Repeticiones" >'+
+								'<input required="" type="Number" class="form-control" id="Repeticion" value="'+Repeticiones+'" name="Repeticion" placeholder="Repeticiones" >'+
 							'</div>'+
 						'</div>'+
 					'<div class="form-group row " >'+
 							'<div class="col-md-6">'+
 								'<label>Descripcion<span class="required-label">*</span></label>'+
-								'<textarea class="form-control" id="Descripcion" name="Descripcion" placeholder="Descripcion" ></textarea>'+
+								'<textarea class="form-control" id="Descripcion" name="Descripcion" value="'+Descripcion+'"  placeholder="Descripcion" ></textarea>'+
 							'</div>'+
 							'<div class="col-md-6">'+
 								'<div class="input-file input-file-image">'+
-									'<img class="img-upload-preview" width="150" src="http://placehold.it/150x150" alt="preview">'+
+									'<img class="img-upload-preview" width="150" src="http://integradora_ultragym_laravel_2019.test/images/FotosRutinas/'+Imagen+'" alt="preview">'+
 									'<input type="file" class="form-control form-control-file" id="uploadImg2" name="uploadImg2" accept="image/jpeg" required="">'+
 									'<label for="uploadImg2" class=" label-input-file btn btn-icon btn-default btn-round btn-lg"><i class="la la-file-image-o"></i> Cargar una Imagen</label>'+
 								'</div>'+
@@ -129,13 +135,16 @@
 					'<div class="card-footer">	'+
 						'<div class="form-group row " >'+
 							'<div class="col-md-12">'+
-								'<center><input  type="submit" class="btn btn-success" id="Repeticiones" name="Repeticiones" value="Registrar"  >'+
+								'<center><input  type="submit" class="btn btn-success" id="Repeticiones" name="Repeticiones" value="Actualizar"  >'+
 								'</center>'+
 							'</div>'+
 						'</div>'+
 					'</div';
 					
 					$('#pintarResultado').html(html);
+					$('#comboRutina').attr("disabled",true);
+
+
 		}
 
 		function mensaje(color,mensaje){
